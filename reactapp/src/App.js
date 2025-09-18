585e33d1e3a6cb8746b0b0655a157d60c8cdf367
Desant <anturated@gmail.com>
2025-09-18 22:14:51 +0200

    add: cors?

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,21 @@
+import {useEffect, useState} from "react";
 import logo from './logo.svg';
 import './App.css';
+// import List from './components/List'
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://anturated-cnepggaggjegayfp.polandcentral-01.azurewebsites.net/api/TestFunction")
+    .then((res) => res.text())
+    .then((data) => setMessage(data));
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>sample text</h1>
+      <p>{message}</p>
     </div>
   );
 }
